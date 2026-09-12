2a44bb4f8edde990cade77c1e114bff3bc96e402
kurtlandrito <kurtulrich.landrito@gmail.com>
2026-09-12 10:58:24 -0500

    fix merge

--- src/server.ts
+++ src/server.ts
@@ -6,5 +6,5 @@ const PORT: string | number = process.env.PORT || 3000;
 const server: Server = app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
-
+// something
 export { server };
